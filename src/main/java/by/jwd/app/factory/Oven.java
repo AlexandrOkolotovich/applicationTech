a047c6e4707cb29c4906e8396ba97b8da2566ead
AlexandrOkolotovich <60757062+AlexandrOkolotovich@users.noreply.github.com>
2020-12-10 10:14:10 +0300

    Add files via upload

--- src/main/java/by/jwd/app/factory/Oven.java
+++ src/main/java/by/jwd/app/factory/Oven.java
@@ -1,5 +1,7 @@
 package by.jwd.app.factory;
 
+import java.util.Objects;
+
 public class Oven implements Technics {
     private int power_consumption;
     private float weight;
@@ -7,6 +9,96 @@ public class Oven implements Technics {
     private float depth;
     private float height;
     private float width;
+
+    public Oven(int power_consumption, float weight, float capacity, float depth, float height, float width) {
+        this.power_consumption = power_consumption;
+        this.weight = weight;
+        this.capacity = capacity;
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Oven() {
+    }
+
+    public int getPower_consumption() {
+        return power_consumption;
+    }
+
+    public void setPower_consumption(int power_consumption) {
+        this.power_consumption = power_consumption;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(float capacity) {
+        this.capacity = capacity;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+
+    public void setDepth(float depth) {
+        this.depth = depth;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Oven oven = (Oven) o;
+        return power_consumption == oven.power_consumption &&
+                Float.compare(oven.weight, weight) == 0 &&
+                Float.compare(oven.capacity, capacity) == 0 &&
+                Float.compare(oven.depth, depth) == 0 &&
+                Float.compare(oven.height, height) == 0 &&
+                Float.compare(oven.width, width) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(power_consumption, weight, capacity, depth, height, width);
+    }
+
+    @Override
+    public String toString() {
+        return "Oven{" +
+                "power_consumption=" + power_consumption +
+                ", weight=" + weight +
+                ", capacity=" + capacity +
+                ", depth=" + depth +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
 
     @Override
     public void setTechnicalSpecifications() {
--- src/main/java/by/jwd/app/factory/Refrigerator.java
+++ src/main/java/by/jwd/app/factory/Refrigerator.java
@@ -1,5 +1,7 @@
 package by.jwd.app.factory;
 
+import java.util.Objects;
+
 public class Refrigerator implements Technics {
     private int power_consumption;
     private float weight;
@@ -7,6 +9,96 @@ public class Refrigerator implements Technics {
     private float overal_capacity;
     private float height;
     private float width;
+
+    public Refrigerator(int power_consumption, float weight, float freezer_capacity, float overal_capacity, float height, float width) {
+        this.power_consumption = power_consumption;
+        this.weight = weight;
+        this.freezer_capacity = freezer_capacity;
+        this.overal_capacity = overal_capacity;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Refrigerator() {
+    }
+
+    public int getPower_consumption() {
+        return power_consumption;
+    }
+
+    public void setPower_consumption(int power_consumption) {
+        this.power_consumption = power_consumption;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getFreezer_capacity() {
+        return freezer_capacity;
+    }
+
+    public void setFreezer_capacity(float freezer_capacity) {
+        this.freezer_capacity = freezer_capacity;
+    }
+
+    public float getOveral_capacity() {
+        return overal_capacity;
+    }
+
+    public void setOveral_capacity(float overal_capacity) {
+        this.overal_capacity = overal_capacity;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Refrigerator that = (Refrigerator) o;
+        return power_consumption == that.power_consumption &&
+                Float.compare(that.weight, weight) == 0 &&
+                Float.compare(that.freezer_capacity, freezer_capacity) == 0 &&
+                Float.compare(that.overal_capacity, overal_capacity) == 0 &&
+                Float.compare(that.height, height) == 0 &&
+                Float.compare(that.width, width) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(power_consumption, weight, freezer_capacity, overal_capacity, height, width);
+    }
+
+    @Override
+    public String toString() {
+        return "Refrigerator{" +
+                "power_consumption=" + power_consumption +
+                ", weight=" + weight +
+                ", freezer_capacity=" + freezer_capacity +
+                ", overal_capacity=" + overal_capacity +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
 
     @Override
     public void setTechnicalSpecifications() {
